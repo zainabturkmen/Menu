2fd71d5995cb843aedf282b10c92275ea900c65e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-19 13:25:21 +0430

    adding some buttons

--- app.js
+++ app.js
@@ -76,7 +76,11 @@ const menu = [
 const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded", function(){
-  let displayMenu = menu.map(function(item){
+  displayMenuItems(menu);
+})
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
 
     return `<article class="menu-item">
     <img src="${item.img}" class="photo" alt="${item.title}">
@@ -91,4 +95,4 @@ window.addEventListener("DOMContentLoaded", function(){
   })
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-})
+}
