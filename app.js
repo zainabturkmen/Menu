94f1c97133ebd13d246ad31b8c990227c9e34d1c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-19 13:15:02 +0430

    setting up

--- app.js
+++ app.js
@@ -77,9 +77,18 @@ const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded", function(){
   let displayMenu = menu.map(function(item){
-    
-    return item;
-  })
 
-  console.log(displayMenu);
+    return `<article class="menu-item">
+    <img src="${item.img}" class="photo" alt="${item.title}">
+    <div class="item info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">${item.desc}</p>
+    </div>
+  </article>`;
+  })
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
 })
